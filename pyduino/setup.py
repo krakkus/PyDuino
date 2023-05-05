46b2a942dd861ee18ea3e9646b847f636816d48f
krakkus <116948352+energy-automation@users.noreply.github.com>
2023-05-05 19:25:17 +0200

    moved readme

--- pyduino/setup.py
+++ pyduino/setup.py
@@ -8,7 +8,7 @@ setup(
     author="Your Name",
     author_email="your.email@example.com",
     description="A Python module to control one or more Arduino connected to a PC",
-    long_description=open("README.md").read(),
+    long_description=open("../README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/pyduino_control",
     classifiers=[
