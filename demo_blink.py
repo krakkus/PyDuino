74c9a348fcfec22fb932c958941d95db0b2f9669
krakkus <45657312+krakkus@users.noreply.github.com>
2024-05-19 11:55:42 +0200

    Update demo_blink.py

--- demo_blink.py
+++ demo_blink.py
@@ -14,12 +14,14 @@ while True:
 
     # Turn the lamp off
     pot = devices['ESP32_1'].digitalWrite(2, 0)
+    print("Light turned on")
 
     # Wait 5 seconds
     time.sleep(5)
 
     # Turn the lamp on
     pot = devices['ESP32_1'].digitalWrite(2, 1)
+    print("Light turned off")
 
     # Wait 5 seconds
     time.sleep(5)
