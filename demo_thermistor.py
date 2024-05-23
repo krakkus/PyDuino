401dd5bcbcf5297d641954d4bcdaa6e9431e379b
krakkus <45657312+krakkus@users.noreply.github.com>
2024-05-23 19:11:24 +0200

    .

---
+++ demo_thermistor.py
@@ -0,0 +1,15 @@
+# Import the PyDuino library
+from pyduino import *
+
+# Discover devices on local subnet and COM ports
+devices = PyDuino.FindAll()
+
+# Show us all discovered devices
+for k, v in devices.items():
+    print(k, v.address)
+
+# esp = devices['ESP32_1']
+#
+# a = esp.analogRead(35)
+#
+# print(a)
